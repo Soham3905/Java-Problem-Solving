d2f3fa10908ae544b3f543879529eb353c227916
Soham <sohamdhakatecse3905@gmail.com>
2026-01-02 14:36:26 +0530

    961

--- Practice.java
+++ Practice.java
@@ -9,4 +9,16 @@ public class Practice {
 
     }
 
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(!set.contains(num)){
+                set.add(num);
+            }else{
+                return num;
+            }
+        }
+        return 0;
+    }
+
 }

a3f25afca745a81cd4aa92aa2cc805f4cd3b4f7c
Soham <sohamdhakatecse3905@gmail.com>
2026-01-11 15:51:56 +0530

    Implement multiple algorithms including residuePrefixes, centeredSubarrays, countPairs, diff, largestRectangleArea, maximalRectangle, and sortArrayByParity in Practice.java

--- Practice.java
+++ Practice.java
@@ -9,72 +9,150 @@ public class Practice {
 
     }
 
-    class Solution {
-    public int minimumDeleteSum(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-
-        int[][] dp = new int[n + 1][m + 1];
-
+    public int residuePrefixes(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int n = s.length();
+        int distinctChar = 0;
+        int result = 0;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
+            if (!set.contains(s.charAt(i - 1))) {
+                set.add(s.charAt(i - 1));
+                distinctChar += 1;
+            }
+            if ((i % 3) == distinctChar) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public int centeredSubarrays(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> set = new HashSet<>();
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                set.add(nums[j]);
+                if (set.contains(sum)) {
+                    result += 1;
+                }
+            }
+        }
+        return result;
+    }
+
+    public long countPairs(String[] words) {
+        HashMap<String, Integer> map = new HashMap<>();
+        long result = 0;
+        for (String word : words) {
+            String hash = diff(word);
+            result += map.getOrDefault(hash, 0);
+            map.put(hash, map.getOrDefault(hash, 0) + 1);
+        }
+        return result;
+    }
+
+    public String diff(String word) {
+        StringBuilder sb = new StringBuilder();
+        int first = word.charAt(0) - 'a';
+        for (char ch : word.toCharArray()) {
+            int difference = (ch - 'a' - first + 26) % 26;
+            sb.append(difference).append("_");
+        }
+        return sb.toString();
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int ans = 0;
+        int n = heights.length;
+
+        int[] rightNearest = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                rightNearest[i] = n;
+            } else {
+                rightNearest[i] = stack.peek();
+            }
+            stack.add(i);
+        }
+        while (!stack.isEmpty()) {
+            stack.pop();
+        }
+        int[] leftNearest = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                leftNearest[i] = -1;
+            } else {
+                leftNearest[i] = stack.peek();
+            }
+            stack.add(i);
+        }
+        for (int i = 0; i < n; i++) {
+            int currArea = heights[i] * (rightNearest[i] - leftNearest[i] - 1);
+            ans = Math.max(ans, currArea);
+        }
+        return ans;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '0') {
+                    arr[i][j] = 0;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    arr[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    arr[i][j] += arr[i - 1][j];
+                } else {
+                    arr[i][j] = 0;
                 }
             }
         }
 
-        int i = n, j = m;
-        String lcs = "";
-
-        while (i > 0 && j > 0) {
-            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                lcs = s1.charAt(i - 1) + lcs;
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                i--;
-            } else {
-                j--;
-            }
+        int result = 0;
+        for (int[] row : arr) {
+            int curr = largestRectangleArea(row);
+            result = Math.max(result, curr);
         }
-
-        int sum = 0;
-        int p = 0, q = 0;
-
-        while (p < n && q < lcs.length()) {
-            if (s1.charAt(p) == lcs.charAt(q)) {
-                p++;
-                q++;
-            } else {
-                sum += s1.charAt(p);
-                p++;
-            }
-        }
-        while (p < n) {
-            sum += s1.charAt(p++);
-        }
-
-        int r = 0, s = 0;
-
-        while (r < m && s < lcs.length()) {
-            if (s2.charAt(r) == lcs.charAt(s)) {
-                r++;
-                s++;
-            } else {
-                sum += s2.charAt(r);
-                r++;
-            }
-        }
-        while (r < m) {
-            sum += s2.charAt(r++);
-        }
-
-        return sum;
+        return result;
     }
-}
 
+    public int[] sortArrayByParity(int[] nums) {
+        int i = 0;
+        int j = nums.length-1;
 
+        while (i<=j) { 
+            if (nums[i]%2!=0 && nums[j]%2==0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }else if (nums[i]%2==0) {
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return nums;
+    }
 }

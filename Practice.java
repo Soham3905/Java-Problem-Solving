d0aa14bc6b06fd7bbf99caaeeacb40605190512c
Soham <sohamdhakatecse3905@gmail.com>
2026-01-18 14:27:55 +0530

    Refactor Practice.java: Remove maximizeSquareHoleArea and related methods; enhance vowelConsonantScore and maxCapacity implementations.

--- Practice.java
+++ Practice.java
@@ -9,208 +9,119 @@ public class Practice {
 
     }
 
-    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
-        Arrays.sort(hBars);
-        Arrays.sort(vBars);
+    public int vowelConsonantScore(String s) {
+        int v = 0, c = 0;
 
-        int ans = Math.min(helper(hBars), helper(vBars));
-        return ans * ans;
-    }
-
-    public int helper(int[] bars) {
-        int result = 1;
-        int count = 1;
-        for (int i = 1; i < bars.length; i++) {
-            if (bars[i] == bars[i - 1] + 1) {
-                count++;
-            } else {
-                count = 1;
-            }
-            result = Math.max(result, count);
-        }
-        return result + 1;
-    }
-
-    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        list1.add(m);
-        for (int ele : hFences) {
-            list1.add(ele);
-        }
-
-        ArrayList<Integer> list2 = new ArrayList<>();
-        list2.add(1);
-        list2.add(n);
-        for (int ele : vFences) {
-            list2.add(ele);
-        }
-
-        long result = 0;
-
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < list1.size(); i++) {
-            for (int j = i + 1; j < list1.size(); j++) {
-                int diff = Math.abs(list1.get(j) - list1.get(i));
-                set.add(diff);
-            }
-        }
-
-        for (int i = 0; i < list2.size(); i++) {
-            for (int j = i + 1; j < list2.size(); j++) {
-                int diff = Math.abs(list2.get(j) - list2.get(i));
-                if (set.contains(diff)) {
-                    result = Math.max(result, diff);
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    v++;
+                } else {
+                    c++;
                 }
             }
         }
 
-        if (result == 0) {
-            return -1;
-        }
-        return (int) ((result * result) % 1_000_000_007);
-    }
-
-    class Pair implements Comparable<Pair> {
-
-        int x;
-        int y;
-        int q;
-
-        public Pair(int x, int y, int q) {
-            this.x = x;
-            this.y = y;
-            this.q = q;
-        }
-
-        @Override
-        public int compareTo(Pair other) {
-            if (this.q != other.q) {
-                return other.q - this.q;
-            }
-            if (this.x != other.x) {
-                return this.x - other.x;
-            }
-            return this.y - other.y;
+        if (c > 0) {
+            return (int) Math.floor(v / c);
+        } else {
+            return 0;
         }
     }
 
-    public int[] bestTower(int[][] towers, int[] center, int radius) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-
-        for (int[] t : towers) {
-            int dist = Math.abs(t[0] - center[0]) + Math.abs(t[1] - center[1]);
-            if (dist <= radius) {
-                pq.add(new Pair(t[0], t[1], t[2]));
-            }
-        }
-
-        if (pq.isEmpty()) {
-            return new int[]{-1, -1};
-        }
-
-        Pair best = pq.poll();
-        return new int[]{best.x, best.y};
-    }
-
-    public int minOperations(int[] nums, int[] target) {
-        int n = nums.length;
-        HashSet<Integer> set = new HashSet<>();
-
+    public int maxCapacity(int[] costs, int[] capacity, int budget) {
+        int n = costs.length;
+        int result = 0;
+        int[][] machines = new int[n][2];
         for (int i = 0; i < n; i++) {
-            if (nums[i] != target[i]) {
-                set.add(nums[i]);
+            machines[i][0] = costs[i];
+            machines[i][1] = capacity[i];
+        }
+        Arrays.sort(machines, (a, b) -> a[0] - b[0]);
+        for (int i = 0; i < n; i++) {
+            if (machines[i][0] < budget) {
+                result = Math.max(machines[i][1], result);
             }
         }
-
-        return set.size();
-    }
-
-    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
-        int n = bottomLeft.length;
-        long ans = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int minX = Math.max(bottomLeft[i][0], bottomLeft[j][0]);
-                int maxX = Math.min(topRight[i][0], topRight[j][0]);
-                int minY = Math.max(bottomLeft[i][1], bottomLeft[j][1]);
-                int maxY = Math.min(topRight[i][1], topRight[j][1]);
-
-                if (minX < maxX && minY < maxY) {
-                    int len = Math.min(maxX - minX, maxY - minY);
-                    ans = Math.max(ans, len);
+                if (costs[i] + costs[j] < budget) {
+                    result = Math.max(result,
+                            capacity[i] + capacity[j]);
                 }
             }
         }
-        return ans * ans;
+        return result;
     }
 
-    public class ListNode {
+    public int largestMagicSquare(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
 
-        int val;
-        ListNode next;
-
-        ListNode() {
+        int[][] rowSum = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            rowSum[i][0] = grid[i][0];
+            for (int j = 1; j < m; j++) {
+                rowSum[i][j] = rowSum[i][j - 1] + grid[i][j];
+            }
         }
 
-        ListNode(int val) {
-            this.val = val;
+        int[][] colSum = new int[n][m];
+        for (int j = 0; j < m; j++) {
+            colSum[0][j] = grid[0][j];
+            for (int i = 1; i < n; i++) {
+                colSum[i][j] = colSum[i - 1][j] + grid[i][j];
+            }
         }
 
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
+        int maxSize = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int maxPossible = Math.min(n - i, m - j);
+                for (int k = maxPossible; k > maxSize; k--) {
+                    if (isMagic(i, j, k, grid, rowSum, colSum)) {
+                        maxSize = k;
+                        break;
+                    }
+                }
+            }
         }
+        return maxSize;
     }
 
-    class Solution {
+    public boolean isMagic(int sr, int sc, int size, int[][] grid, int[][] rowSum, int[][] colSum) {
+        int target = rowSum[sr][sc + size - 1] - (sc > 0 ? rowSum[sr][sc - 1] : 0);
 
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ArrayList<Integer> list1 = new ArrayList<>();
-            ArrayList<Integer> list2 = new ArrayList<>();
-
-            ListNode temp = l1;
-            while (temp != null) {
-                list1.add(temp.val);
-                temp = temp.next;
+        for (int i = sr; i < sr + size; i++) {
+            int sum = rowSum[i][sc + size - 1] - (sc > 0 ? rowSum[i][sc - 1] : 0);
+            if (sum != target) {
+                return false;
             }
-
-            temp = l2;
-            while (temp != null) {
-                list2.add(temp.val);
-                temp = temp.next;
-            }
-
-            Collections.reverse(list1);
-            Collections.reverse(list2);
-
-            StringBuilder str1 = new StringBuilder();
-            StringBuilder str2 = new StringBuilder();
-
-            for (int x : list1) {
-                str1.append(x);
-            }
-            for (int x : list2) {
-                str2.append(x);
-            }
-
-            long a = Long.parseLong(str1.toString());
-            long b = Long.parseLong(str2.toString());
-            long c = a + b;
-
-            String str3 = Long.toString(c);
-
-            ListNode dummy = new ListNode(0);
-            ListNode curr = dummy;
-
-            for (int i = str3.length() - 1; i >= 0; i--) {
-                curr.next = new ListNode(str3.charAt(i) - '0');
-                curr = curr.next;
-            }
-
-            return dummy.next;
         }
+
+        for (int j = sc; j < sc + size; j++) {
+            int sum = colSum[sr + size - 1][j] - (sr > 0 ? colSum[sr - 1][j] : 0);
+            if (sum != target) {
+                return false;
+            }
+        }
+
+        int d1 = 0;
+        for (int k = 0; k < size; k++) {
+            d1 += grid[sr + k][sc + k];
+        }
+        if (d1 != target) {
+            return false;
+        }
+
+        int d2 = 0;
+        for (int k = 0; k < size; k++) {
+            d2 += grid[sr + size - 1 - k][sc + k];
+        }
+
+        return d2 == target;
     }
 
 }

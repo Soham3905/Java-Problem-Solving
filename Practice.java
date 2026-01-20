6e63010fc03dd0dd996048abf4e9f5b22d4108f0
Soham <sohamdhakatecse3905@gmail.com>
2026-01-20 20:56:49 +0530

    3314

--- Practice.java
+++ Practice.java
@@ -9,49 +9,22 @@ public class Practice {
 
     }
 
-    public int maxSideLength(int[][] mat, int threshold) {
-        int n = mat.length;
-        int m = mat[0].length;
-
-        int[][] rowSum = new int[n][m];
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
-            rowSum[i][0] = mat[i][0];
-            for (int j = 1; j < m; j++) {
-                rowSum[i][j] = rowSum[i][j - 1] + mat[i][j];
-            }
-        }
-
-        int[][] colSum = new int[n][m];
-        for (int j = 0; j < m; j++) {
-            colSum[0][j] = mat[0][j];
-            for (int i = 1; i < n; i++) {
-                colSum[i][j] = colSum[i - 1][j] + mat[i][j];
-            }
-        }
-
-        int maxSize = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int maxPossible = Math.min(n - i, m - j);
-                for (int k = maxPossible; k > maxSize; k--) {
-                    if (helper(i, j, k, mat, rowSum, colSum, threshold)) {
-                        maxSize = k;
-                        break;
-                    }
+            int ele = nums.get(i);
+            for (int j = 0; j < ele; j++) {
+                if ((j | (j + 1))==nums.get(i)) {
+                    ans[i] = j;
+                    break;
                 }
             }
+            if (ans[i]==0) {
+                ans[i] = -1;
+            }
         }
-        return maxSize;
-    }
-
-    public boolean helper(int sr, int sc, int size, int[][] grid, int[][] rowSum, int[][] colSum, int threshold) {
-        int result = 0;
-        for (int i = sr; i < sr + size; i++) {
-            int sum = rowSum[i][sc + size - 1] - (sc > 0 ? rowSum[i][sc - 1] : 0);
-            result += sum;
-        }
-        return result <= threshold;
+        return ans;
     }
 
 }

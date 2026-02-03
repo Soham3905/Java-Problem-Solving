2974cdc4101fa0ad9deeaca4871314eb2cda44ae
Soham <sohamdhakatecse3905@gmail.com>
2026-02-03 19:25:26 +0530

    3637. Trionic Array I

--- Practice.java
+++ Practice.java
@@ -9,52 +9,82 @@ public class Practice {
 
     }
 
-    public String reverseByType(String s) {
-        Stack<Character> stack1 = new Stack<>();
-        Stack<Character> stack2 = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (Character.isLowerCase(ch)) {
-                stack1.push(ch);
-            } else {
-                stack2.push(ch);
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (char ch : s.toCharArray()) {
-            if (Character.isLowerCase(ch)) {
-                sb.append(stack1.pop());
-            } else {
-                sb.append(stack2.pop());
-            }
-        }
-        return sb.toString();
-    }
-
-    public int minimumK(int[] nums) {
-        int n = nums.length;
-        int result = -1;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, nums[i]);
-        }
-
-        int start = 1;
-        int end = max;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            long count = 0;
-            for (int ele : nums) {
-                count += Math.ceil(ele / mid);
-            }
-            if (count <= (long) mid * mid) {
-                result = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+        int index = 0;
+        for(int i = -n/2 ; i <= n/2 ; i++){
+            if(n%2==0 && i==0){
+                continue;
+            }else{
+                result[index] = i;
+                index++;
             }
         }
         return result;
+    }
+
+    public boolean helper(int ele){
+        while(ele>0){
+            int rem = ele%10;
+            if(rem==0){
+                return false;
+            }
+            ele /= 10;
+        }
+        return true;
+    }
+
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i <= n/2; i++) {
+            if(helper(i) && helper(n-i)){
+                return new int[]{i,n-i};
+            }
+        }
+        return new int[]{1,n-1};
+    }
+
+    public long minimumCost(int[] nums, int k, int dist) {
+        int n = nums.length;
+        if(dist==1){
+            return nums[0]+nums[1]+nums[2];
+        }
+        long result = 0;
+        for (int i = 1; i < n-dist; i++) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            int sum = 0;
+            for (int j = i; j < i+dist; j++) {
+                pq.add(nums[j]);
+            }
+            while (k-1>0) { 
+                sum+=pq.poll();
+                k--;
+            }
+            result = Math.min(result,sum);
+        }
+        return nums[0]+result;
+    }
+
+        public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        boolean a = false;
+        boolean b = false;
+        boolean c = false;
+
+        int i = 0;
+
+        while(i+1<n && nums[i]<nums[i+1]){
+            a = true;
+            i++;
+        }
+        while(i+1<n && nums[i]>nums[i+1]){
+            b = true;
+            i++;
+        }
+        while(i+1<n && nums[i]<nums[i+1]){
+            c = true;
+            i++;
+        }
+        return a && b && c && i==n-1;
     }
 
 }

2bd72bf3a6f36658cc8a1252a377f38a565de2cd
Soham <sohamdhakatecse3905@gmail.com>
2026-02-09 20:40:38 +0530

    Daily problem 1382

--- Practice.java
+++ Practice.java
@@ -9,19 +9,46 @@ public class Practice {
 
     }
 
-    public int[] constructTransformedArray(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-           if (nums[i]>0) {
-               result[i] = nums[(i+nums[i])%n];
-           }else if(nums[i]<0){
-            result[i] = nums[(i+(nums[i]+n))%n];
-           }else{
-            result[i] = nums[i];
-           }
-        }
-        return result;
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(root,list);
+        return solve(0,list.size()-1,root,list);
     }
+
+    public TreeNode solve(int left, int right, TreeNode root,ArrayList<Integer> list){
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right-left)/2;
+        TreeNode node = new TreeNode(list.get(mid));
+        node.left = solve(left, mid-1, root, list);
+        node.right = solve(mid+1, right, root, list);
+        return node;
+    }
+
+    public void inOrder(TreeNode root, ArrayList<Integer> list){
+        if(root == null){
+            return;
+        }
+
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+
+}
 
 }

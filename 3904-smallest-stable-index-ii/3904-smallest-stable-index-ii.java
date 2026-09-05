d0d86f2a6f1ee3c1f644fd7e745885842f913be0
Soham Dhakate <163649741+Soham3905@users.noreply.github.com>
2026-09-05 11:52:11 +0530

    Time: 5 ms (66.41%), Space: 126.9 MB (48.85%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -6,12 +6,12 @@ class Solution {
         int[] MAX = new int[n];
         int[] MIN = new int[n];
         for (int i = 0; i < n; i++) {
-            MAX[i] = Math.max(nums[i], max);
             max = Math.max(nums[i], max);
+            MAX[i] = max;
         }
-        for (int i = n-1; i >= 0; i--) {
-            MIN[i] = Math.min(nums[i], min);
+        for (int i = n - 1; i >= 0; i--) {
             min = Math.min(nums[i], min);
+            MIN[i] = min;
         }
         for (int i = 0; i < n; i++) {
             if ((MAX[i] - MIN[i]) <= k) {
